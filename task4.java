dbbccec6ca4b297d2aa700850686c6e19571acc9
Oleg <olegashepelev@gmail.com>
2023-04-19 00:39:18 +0600

    Complete fourth task

---
+++ task4.java
@@ -0,0 +1,30 @@
+import java.io.FileWriter;
+
+public class task4 {
+    public static void main(String[] args) {
+        ex4("Test", 20);
+    }
+
+    static void ex4(String s, int n) {
+        String s1 = repeaString(s, n);
+        saveToFile(s1);
+    }
+
+    static void saveToFile(String s) {
+        String path = "test.txt";
+        try (FileWriter fileWriter = new FileWriter(path, true)) {
+            fileWriter.write(s);
+            fileWriter.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static String repeaString(String s, int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            stringBuilder.append(s);
+        }
+        return stringBuilder.toString();
+    }
+}
